44cd49c17b9e5dccb653fcc22802069a926002a1
Duncan Dickinson <duncan@dickinson.name>
2015-07-18 14:08:36 +1000

    Added test

---
+++ src/test/groovy/com/github/groovyguides/shapes/measurement/LengthMeasurementMultiplyTest.groovy
@@ -0,0 +1,97 @@
+package com.github.groovyguides.shapes.measurement
+
+import com.github.groovyguides.shapes.measurement.UnitOfLength as UoM
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ *
+ *
+ * @author Duncan Dickinson
+ */
+@Unroll
+class LengthMeasurementMultiplyTest extends Specification {
+    def "Multiplying a measurement (#left) by a number (#right) results in a measurement with value #answer"() {
+        given: "Two measurements with the same unitOfLength of measurement"
+        LengthMeasurement lhs = new LengthMeasurement(left, UoM.Millimetre)
+        Number rhs = right
+
+        when: "they are divided"
+        LengthMeasurement result = lhs * rhs
+
+        then: "the result should equal the length held in the answer"
+        result.length == answer
+
+        where: "the left and right measurement values and the answer are"
+        left | right || answer
+        10 | 5 || 50
+        15 | 10 || 150
+        99 | 9 || 891
+        8 | 2 || 16
+    }
+
+    def "Multiplying two measurements (#left, #right) results in a measurement with value #answer"() {
+        given: "Two measurements with the same unitOfLength of measurement"
+        LengthMeasurement lhs = new LengthMeasurement(left, UoM.Millimetre)
+        LengthMeasurement rhs = new LengthMeasurement(right, UoM.Millimetre)
+
+        when: "they are divided"
+        LengthMeasurement result = lhs * rhs
+
+        then: "the result should equal the length held in the answer"
+        result.length == answer
+
+        where: "the left and right measurement values and the answer are"
+        left | right || answer
+        10 | 5 || 50
+        15 | 10 || 150
+        99 | 9 || 891
+        8 | 2 || 16
+    }
+
+    def "Attempting to multiply a measurement by a number <= 0 (#right) throws an IllegalArgumentException"() {
+        given: "One measurement and a Number"
+        LengthMeasurement lhs = new LengthMeasurement(left, UnitOfLength.Inch)
+        Integer rhs = right
+
+        when: "The length of the RHS operand is <=0"
+        Exception e = null
+        try {
+            lhs * rhs
+        } catch (any) {
+            e = any
+        }
+
+        then: "an IllegalArgumentException is thrown"
+            e instanceof IllegalArgumentException
+
+        where: "the left and right measurement values are"
+        left    | right
+        10      | -5
+        15      | -10
+        9       | -9
+        8       | -7
+    }
+
+    def "Attempting to multiply two measurements (#left, #right) with different UoM throws an IllegalArgumentException"() {
+        given: "One measurement and a Number"
+            def lhs = left
+            def rhs = right
+
+        when: "we multiply the two measurements with different UoM"
+            Exception e = null
+            try {
+                lhs * rhs
+            } catch (any) {
+                e = any
+            }
+
+        then: "an IllegalArgumentException is thrown"
+            e instanceof IllegalArgumentException
+
+        where: "the left and right measurement values are"
+            left | right
+            new LengthMeasurement(10, UoM.Millimetre) | new LengthMeasurement(10, UoM.Inch)
+            new LengthMeasurement(10, UoM.Inch) | new LengthMeasurement(10, UoM.Millimetre)
+    }
+}
