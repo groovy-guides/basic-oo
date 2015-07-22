b0349b783d6ba1ac6562d9eacf3c3f8ca84f9770
Duncan Dickinson <duncan@dickinson.name>
2015-07-22 01:05:34 +0000

    Tidied up build error

--- src/main/groovy/com/github/groovyguides/shapes/app/Cli.groovy
+++ src/main/groovy/com/github/groovyguides/shapes/app/Cli.groovy
@@ -12,22 +12,26 @@ class Cli {
     static private final CliBuilder CB = new CliBuilder(usage:'')
 
     ; {
-        cli.with {
+        CB.with {
             h longOpt:'help', 'Show usage information'
             v longOpt:'version', 'Show the current version of the application'
         }
     }
+  
+  static void cliError(text) {
+		System.err << text + "\n"
+		System.exit(1)
+	}
 
     static void main(args) {
         OptionAccessor options = CB.parse(args)
 
         if (!options) {
-            cli_err('Error: No options provided')
-            System.exit(1)
+            cliError('Error: No options provided')
         }
 
         if (options.h) {
-            cli.usage()
+            CB.usage()
             System.exit(0)
         }
 
